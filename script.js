90dfe7d7c5837a06761c3b02ff11d61273592720
Ganesh-Babu-GB <42582114+ganeshbabugb@users.noreply.github.com>
2022-08-21 12:57:52 +0530

    added files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var header = new Typed("#header", {
+  strings: ["Hey There!" ,"I'm Ganesh"],
+  typeSpeed: 40,
+  backSpeed: 20,
+  backDelay: 1000,
+  showCursor: false,
+  loop: true,
+  loopCount: 4,
+});
+
+var footer = new Typed("#footer", {
+  strings: ["Thank you!"],
+  typeSpeed: 40,
+  backSpeed: 20,
+  backDelay: 1000,
+  showCursor: false,
+  loop: true,
+  loopCount: 5,
+});
+
